dc29afc9265e4ad2ff8683ac0d56d137ec6ce34f
jackpoit <jackpoit_able@outlook.com>
2021-06-04 00:26:39 +0800

    copeof

--- src/itwn/com/test01/Test01.java
+++ src/itwn/com/test01/Test01.java
@@ -1,9 +1,17 @@
 package itwn.com.test01;
 
+import java.math.BigDecimal;
+import java.util.Arrays;
+
 public abstract class Test01 {
 	public static void main(String[] args) {
-		String str="accacca";
-		System.out.println(str.replace("a","b"));
+		int[] arr= {10,80,30,20,100};
+		int[] arr2=Arrays.copyOf(arr,10);
+		arr2[1]=1000;
+		System.out.println(Arrays.toString(arr2));
+		System.out.println(Arrays.toString(arr));
+
+
 
 	}
 }
