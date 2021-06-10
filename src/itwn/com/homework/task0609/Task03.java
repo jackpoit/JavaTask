fb15f60130d3e08617c673359bea948ad56c555c
taritari <jackpoit_able@outlook.com>
2021-06-10 11:56:50 +0800

    problem aslist

--- src/itwn/com/homework/task0609/Task03.java
+++ src/itwn/com/homework/task0609/Task03.java
@@ -1,10 +1,23 @@
 package itwn.com.homework.task0609;
 
-import java.util.ArrayList;
+import com.sun.deploy.util.ArrayUtil;
+import javafx.print.Collation;
+
+import java.util.*;
 
 public class Task03 {
 	public static void main(String[] args) {
+
+
+
 		String str="rqwasferwqtgreqqwezxdasg";
+//		char[] c=str.toCharArray();
+		String[] c={"a","b","c"};
+		List<String> s=Arrays.asList(c);
+//		ArrayList<String> s=new ArrayList<>(Arrays.asList(c));
+		System.out.println(s);
+		Collections.addAll(s,c);
+		s.addAll(Arrays.asList(c));
 		ArrayList<String> strList=new ArrayList<>();
 		for (int i=0;i<str.length();i++){
 			if (!strList.contains(str.charAt(i)+"")) {
